f362811eaca93e8d6aa72a471c0a5ed8f7a40057
kacie hu <kaciehu@kacies-mac.local>
2024-11-09 21:26:48 -0800

    login page

---
+++ src/app/login/page.js
@@ -0,0 +1,45 @@
+import './loginpage.css';
+
+export default function LoginPage() {
+    return (
+        <div className="login-container">
+            <form action="/api/login" method="post" className="login-form">
+                
+                {/* Logo */}
+                <div className="login-logo">
+                    <img src="/logo.png" alt="Logo" style={{ width: '50px', height: 'auto' }} />
+                </div>
+                
+                <h2>Welcome back!</h2>
+
+                <div style={{ marginBottom: '1em', textAlign: 'left', marginTop: '20px' }}>
+                    <label>Email:</label>
+                    <input
+                        type="email"
+                        name="email"
+                        required
+                        style={{ width: '100%', padding: '8px', backgroundColor:'#FFFFFF', borderRadius: '12px', marginTop: '7px'}}
+                    />
+                </div>
+
+                <div style={{ marginBottom: '1em', textAlign: 'left', marginTop: '10px' }}>
+                    <label>Password:</label>
+                    <input
+                        type="password"
+                        name="password"
+                        required
+                        style={{ width: '100%', padding: '8px',  backgroundColor:'#FFFFFF', borderRadius: '12px', marginTop: '7px', marginBottom: '10px'}}
+                    />
+                </div>
+
+                <button type="submit" className="login-button">Login</button>
+
+                <div style={{ textAlign: 'center', marginTop: '1em'}}>
+                    <a href="/forgot-password" className="forgot-password-link">
+                        Forgot Password?
+                    </a>
+                </div>
+            </form>
+        </div>
+    );
+}
