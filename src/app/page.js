1de5d470e8d0773af89d4702300684ad65a6d774
Chloe Chang <147461155+choleflour@users.noreply.github.com>
2024-11-10 01:25:49 -0800

    changed main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,160 +1,163 @@
 'use client';
 
-import Image from "next/image";
+// import Image from "next/image";
 // import styles from "./page.module.css";
-import { createUserWithEmailAndPassword,signInWithEmailAndPassword} from "firebase/auth";
-import { useState } from "react";
+// import { createUserWithEmailAndPassword,signInWithEmailAndPassword} from "firebase/auth";
+// import { useState } from "react";
 
 export default function Home() {
-  const [userEmail, setUserEmail] = useState('');
-  const [userPass, setUserPass] = useState('');
-  // const userEmail = useState(''); // => [value of variable, function to set that variable]
-
-  function signUp() {
-    console.log(userEmail);
-
-    if(!userEmail.length) {
-      return;
-    }
-
-    createUserWithEmailAndPassword(auth, userEmail, userPass)
-    .then((userCredential) => {
-      const user = userCredential.user;
-      // ...
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-    });
-  }
-
-
-  function signIn() {
-    signInWithEmailAndPassword(auth, userEmail, userPass)
-    .then((userCredential) => {
-      // Signed in 
-      const user = userCredential.user;
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-    });
-  }
-  function authInfo() {
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        // User is signed in, see docs for a list of available properties
-        // https://firebase.google.com/docs/reference/js/auth.user
-        const uid = user.uid;
-        console.log(uid);
-        // ...
-      } else {
-        // User is signed out
-        // ...
-      }
-    });
-  }
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-    {userEmail}
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
+    <div>HELLO</div>
+  );
+  // const [userEmail, setUserEmail] = useState('');
+  // const [userPass, setUserPass] = useState('');
+  // // const userEmail = useState(''); // => [value of variable, function to set that variable]
 
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-        <div>
-          <input type="text" placeholder="email" value={userEmail} onChange={function (event) {
-            setUserEmail(event.target.value)
-          }}></input>
+  // function signUp() {
+  //   console.log(userEmail);
 
-          <input type="text" placeholder="password" value={userPass} onChange={function (event) {
-            setUserPass(event.target.value)
-          }}></input>
-          {/* <input type="text" placeholder="email" id="user-input"></input> */}
-          <button onClick={signUp}>Sign Up</button>
-          <button onClick={signIn}>Sign In</button>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  //   if(!userEmail.length) {
+  //     return;
+  //   }
+
+  //   createUserWithEmailAndPassword(auth, userEmail, userPass)
+  //   .then((userCredential) => {
+  //     const user = userCredential.user;
+  //     // ...
+  //   })
+  //   .catch((error) => {
+  //     const errorCode = error.code;
+  //     const errorMessage = error.message;
+  //   });
+  // }
+
+
+  // function signIn() {
+  //   signInWithEmailAndPassword(auth, userEmail, userPass)
+  //   .then((userCredential) => {
+  //     // Signed in 
+  //     const user = userCredential.user;
+  //   })
+  //   .catch((error) => {
+  //     const errorCode = error.code;
+  //     const errorMessage = error.message;
+  //   });
+  // }
+  // function authInfo() {
+  //   onAuthStateChanged(auth, (user) => {
+  //     if (user) {
+  //       // User is signed in, see docs for a list of available properties
+  //       // https://firebase.google.com/docs/reference/js/auth.user
+  //       const uid = user.uid;
+  //       console.log(uid);
+  //       // ...
+  //     } else {
+  //       // User is signed out
+  //       // ...
+  //     }
+  //   });
+  // }
+  // return (
+  //   <div className={styles.page}>
+  //     <main className={styles.main}>
+  //       <Image
+  //         className={styles.logo}
+  //         src="/next.svg"
+  //         alt="Next.js logo"
+  //         width={180}
+  //         height={38}
+  //         priority
+  //       />
+  //       <ol>
+  //         <li>
+  //           Get started by editing <code>src/app/page.js</code>.
+  //         </li>
+  //         <li>Save and see your changes instantly.</li>
+  //       </ol>
+  //   {userEmail}
+  //       <div className={styles.ctas}>
+  //         <a
+  //           className={styles.primary}
+  //           href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+  //           target="_blank"
+  //           rel="noopener noreferrer"
+  //         >
+  //           <Image
+
+  //             className={styles.logo}
+  //             src="/vercel.svg"
+  //             alt="Vercel logomark"
+  //             width={20}
+  //             height={20}
+  //           />
+  //           Deploy now
+  //         </a>
+  //         <a
+  //           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+  //           target="_blank"
+  //           rel="noopener noreferrer"
+  //           className={styles.secondary}
+  //         >
+  //           Read our docs
+  //         </a>
+  //       </div>
+  //       <div>
+  //         <input type="text" placeholder="email" value={userEmail} onChange={function (event) {
+  //           setUserEmail(event.target.value)
+  //         }}></input>
+
+  //         <input type="text" placeholder="password" value={userPass} onChange={function (event) {
+  //           setUserPass(event.target.value)
+  //         }}></input>
+  //         {/* <input type="text" placeholder="email" id="user-input"></input> */}
+  //         <button onClick={signUp}>Sign Up</button>
+  //         <button onClick={signIn}>Sign In</button>
+  //       </div>
+  //     </main>
+  //     <footer className={styles.footer}>
+  //       <a
+  //         href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         <Image
+  //           aria-hidden
+  //           src="/file.svg"
+  //           alt="File icon"
+  //           width={16}
+  //           height={16}
+  //         />
+  //         Learn
+  //       </a>
+  //       <a
+  //         href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         <Image
+  //           aria-hidden
+  //           src="/window.svg"
+  //           alt="Window icon"
+  //           width={16}
+  //           height={16}
+  //         />
+  //         Examples
+  //       </a>
+  //       <a
+  //         href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         <Image
+  //           aria-hidden
+  //           src="/globe.svg"
+  //           alt="Globe icon"
+  //           width={16}
+  //           height={16}
+  //         />
+  //         Go to nextjs.org →
+  //       </a>
+  //     </footer>
+  //   </div>
   );
 }
