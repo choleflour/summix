edde780e7046ab69327f951273f004080916c5e5
Chloe Chang <147461155+choleflour@users.noreply.github.com>
2024-11-09 19:59:46 -0800

    uhhh just ... any.. sign in? sure. sure.

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,89 @@
+'use client';
+
 import Image from "next/image";
 import styles from "./page.module.css";
+import "firebase/firestore"
+import "firebase/auth"
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+import { getAuth, createUserWithEmailAndPassword,signInWithEmailAndPassword} from "firebase/auth";
+import { useState } from "react";
+
+
+// Import the functions you need from the SDKs you need
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyADgWsMVr2ET62sAnXM8mvUUKQbXBXoXoQ",
+  authDomain: "summix-9eff0.firebaseapp.com",
+  projectId: "summix-9eff0",
+  storageBucket: "summix-9eff0.firebasestorage.app",
+  messagingSenderId: "468955979293",
+  appId: "1:468955979293:web:1683a181ad3e96f1ba30f4",
+  measurementId: "G-H4RBT56T2R"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const auth = getAuth();
+
 
 export default function Home() {
+  const [userEmail, setUserEmail] = useState('');
+  const [userPass, setUserPass] = useState('');
+  // const userEmail = useState(''); // => [value of variable, function to set that variable]
+
+  function signUp() {
+    console.log(userEmail);
+
+    if(!userEmail.length) {
+      return;
+    }
+
+    createUserWithEmailAndPassword(auth, userEmail, userPass)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
+  }
+
+
+  function signIn() {
+    signInWithEmailAndPassword(auth, userEmail, userPass)
+    .then((userCredential) => {
+      // Signed in 
+      const user = userCredential.user;
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
+  }
+  function authInfo() {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/auth.user
+        const uid = user.uid;
+        console.log(uid);
+        // ...
+      } else {
+        // User is signed out
+        // ...
+      }
+    });
+  }
   return (
     <div className={styles.page}>
       <main className={styles.main}>
@@ -19,7 +101,7 @@ export default function Home() {
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
-
+    {userEmail}
         <div className={styles.ctas}>
           <a
             className={styles.primary}
@@ -28,6 +110,7 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             <Image
+
               className={styles.logo}
               src="/vercel.svg"
               alt="Vercel logomark"
@@ -44,6 +127,18 @@ export default function Home() {
           >
             Read our docs
           </a>
+        </div>
+        <div>
+          <input type="text" placeholder="email" value={userEmail} onChange={function (event) {
+            setUserEmail(event.target.value)
+          }}></input>
+
+          <input type="text" placeholder="password" value={userPass} onChange={function (event) {
+            setUserPass(event.target.value)
+          }}></input>
+          {/* <input type="text" placeholder="email" id="user-input"></input> */}
+          <button onClick={signUp}>Sign Up</button>
+          <button onClick={signIn}>Sign In</button>
         </div>
       </main>
       <footer className={styles.footer}>
